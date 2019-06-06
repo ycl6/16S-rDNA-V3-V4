e0a64c3343c7e1992e1e4a96091e9348199dd986
I-Hsuan Lin <ycl6.gel@gmail.com>
2019-06-06 23:19:19 +0800

    Update lefse-analysis.R

--- lefse-analysis.R
+++ lefse-analysis.R
@@ -37,3 +37,30 @@ write.table(lefse1, file="lefse/expr1.lefse_table.txt", sep = "\t", quote = F, r
 # /path-to-script/graphlan_commit_6ca8735/graphlan.py --dpi 150 lefse/expr1.graphlan_outtree.txt lefse/expr1.graphlan.png --external_legends --size 8 --pad 0.2
 # perl /path-to-script/lefse.pl lefse/expr1.lefse_table.res lefse/expr1.graphlan_outtree.txt lefse/expr1.out
 #######################################################
+
+# plot_res in R
+library(grid)
+Sys.setlocale("LC_COLLATE","C")
+
+res1 = data.frame(fread("lefse/expr1.out"))
+res1 = res1[order(res1$order),]
+names(res1)[5] = "Group"
+res1$taxon = paste0(res1$taxon, "(",res1$rank, ";", " ",res1$order, ")")
+res1$taxon = as.factor(res1$taxon)
+res1$taxon = factor(res1$taxon, levels = unique(res1$taxon[order(res1$order, decreasing=T)]))
+res1$Group = as.factor(res1$Group)
+levels(res1$taxon) = gsub("Escherichia_Shigella","Escherichia/Shigella",levels(res1$taxon))
+levels(res1$taxon) = gsub("_UCG_","_UCG-",levels(res1$taxon))
+
+plot_lefse = ggplot(res1, aes(taxon, lda, fill = Group)) + geom_bar(stat = "identity", width = 0.7, size = 0.5) + coord_flip() + theme_bw() +
+facet_wrap(~ Group, ncol = 1, scales = "free_y") + theme(legend.position="right", axis.text.x=element_text(size = 12), axis.text.y=element_text(face = "bold", size = 8),
+strip.text.x = element_text(face = "bold", size = 12)) + labs(title = "Linear discriminant analysis (LDA)\nEffect Size Analysis", x = "Taxon", y = "LDA")
+groupN = res1 %>% group_by(Group) %>% summarise(count = length(unique(taxon)))
+gt = ggplotGrob(plot_lefse)
+panelI.1 <- gt$layout$t[grepl("panel", gt$layout$name)]
+gt$heights[panelI.1] <- unit(groupN$count,"null")
+dev.off()
+
+png("lefse/expr1.lefse_table.png", width = 8, height = 8, units = "in", res = 300)
+grid.draw(gt)
+dev.off()
