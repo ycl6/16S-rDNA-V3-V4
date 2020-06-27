639f7353d86a62c1c65196d53d433397030c02e1
I-Hsuan Lin <ycl6.gel@gmail.com>
2020-06-27 14:27:14 +0100

    Remove redundant code

--- run_trimming.pl
+++ run_trimming.pl
@@ -68,13 +68,6 @@ foreach my $f (glob("$path/$raw/*_R1_001.fastq.gz")) {
 	print "Running cutadapt on Sample $nid\n";
 
 	$opt = "--match-read-wildcards --no-indels --error-rate 0.15 --overlap 8";	# Maximum error rate 0.15; Minimum overlap 8
-	$cmd = "cutadapt $opt --discard-untrimmed -g $primerF -o $tmp/$nid.01.fastq.gz -p $tmp/$nid.02.fastq.gz $fq1 $fq2 > $log";
-	print "$cmd\n";
-	system($cmd);
-	$cmd = "cutadapt $opt --discard-untrimmed -g $primerR -o $tmp/$nid.12.fastq.gz -p $tmp/$nid.11.fastq.gz $tmp/$nid.02.fastq.gz $tmp/$nid.01.fastq.gz >> $log";
-	print("$cmd\n");
-	system($cmd);
-
 	$cmd = "cutadapt $opt -g $primerF -o $tmp/$nid.01.fastq.gz -p $tmp/$nid.02.fastq.gz $fq1 $fq2 > $log";
 	print "$cmd\n";
 	system($cmd);
