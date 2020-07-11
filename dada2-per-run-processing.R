1af64c9e5f66a9db9abcc14950a6c6dc9d25b236
I-Hsuan Lin <ycl6.gel@gmail.com>
2020-07-11 17:09:27 +0100

    Revert "Revert "Use learnErrors function""
    This reverts commit 7e3e6a23f9e89d1c5503fa0250cd1664625cf80b.

--- dada2-per-run-processing.R
+++ dada2-per-run-processing.R
@@ -2,21 +2,23 @@
 
 library("dada2")
 library("ggplot2")
-options(width=190)
+options(width = 190)
 
 fastq = "trimmed"
 filt = "filt"
 
-fns <- sort(list.files(fastq, full.names = TRUE))
-fnFs <- fns[grep(".1.fastq.gz", fns)]
-fnRs <- fns[grep(".2.fastq.gz", fns)]
+fns = sort(list.files(fastq, full.names = TRUE))
+fnFs = fns[grep(".1.fastq.gz", fns)]
+fnRs = fns[grep(".2.fastq.gz", fns)]
+sample.names = gsub(".1.fastq.gz", "", basename(fnFs))
 
 # Plot quality profile of fastq files
-ii = 1:length(fnFs)
-pdf("plotQualityProfile.pdf", width=10, height=10, pointsize=12)
+ii = 1:length(sample.names)
+pdf("plotQualityProfile.pdf", width = 8, height = 8, pointsize = 12)
 for(i in ii) {
-        print(plotQualityProfile(fnFs[i]) + ggtitle("Fwd"))
-        print(plotQualityProfile(fnRs[i]) + ggtitle("Rev"))
+	message(paste0("[", i ,"/", length(sample.names), "] ", sample.names[i]))
+	print(plotQualityProfile(fnFs[i]) + ggtitle("Fwd"))
+	print(plotQualityProfile(fnRs[i]) + ggtitle("Rev"))
 }
 dev.off()
 
@@ -27,38 +29,38 @@ filtRs <- file.path(filt, basename(fnRs))
 
 # Filtering and trimming
 # Review "plotQualityProfile.pdf" to select the best paramters for truncLen
-out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, 
+out = filterAndTrim(fnFs, filtFs, fnRs, filtRs, 
                      # Need to keep paramters consistent between runs of the same study
-                     truncLen=c(260,200), minLen=200, maxN=0, truncQ=2, maxEE=c(2,5),
-                     rm.phix=TRUE, compress=TRUE, verbose=TRUE, multithread=TRUE)
-#head(out)
+                     truncLen = c(260,200), minLen = 200, maxN = 0, truncQ = 2, maxEE = c(2,5),
+                     rm.phix = TRUE, compress = TRUE, verbose = TRUE, multithread = TRUE)
 
-# Dereplication
-derepFs <- derepFastq(filtFs, verbose=TRUE)
-derepRs <- derepFastq(filtRs, verbose=TRUE)
-names(derepFs) <- gsub(".1.fastq.gz","",names(derepFs)) # Change sample name
-names(derepRs) <- gsub(".2.fastq.gz","",names(derepRs)) # Change sample name
+out = as.data.frame(out)
+rownames(out) = sample.names
+head(out, 10)
 
 # Learn the Error Rates
-dadaFs.lrn <- dada(derepFs, err=NULL, selfConsist = TRUE, multithread=TRUE)
-dadaRs.lrn <- dada(derepRs, err=NULL, selfConsist = TRUE, multithread=TRUE)
+# The derepFastq function used in past workflow has been intergrated into learnErrors function
+errF = learnErrors(filtFs, multithread = TRUE)
+errR = learnErrors(filtRs, multithread = TRUE)
 
-pdf("plotErrors.pdf", width=10, height=10, pointsize=12)
-plotErrors(dadaFs.lrn[[1]], nominalQ=TRUE)
-plotErrors(dadaRs.lrn[[1]], nominalQ=TRUE)
+pdf("plotErrors.pdf", width = 10, height = 10, pointsize = 12)
+plotErrors(errF, nominalQ = TRUE)
+plotErrors(errR, nominalQ = TRUE)
 dev.off()
 
-# Pooled sample Inference
-errF <- dadaFs.lrn[[1]]$err_out
-dadaFs <- dada(derepFs, err=errF, pool=TRUE, multithread=TRUE)
-errR <- dadaRs.lrn[[1]]$err_out
-dadaRs <- dada(derepRs, err=errR, pool=TRUE, multithread=TRUE)
+# Sample Inference
+# By default, the `dada` function processes each sample independently (pool = FALSE)
+# Use `pool = TRUE` or `pool = pseudo` (recommended) if samples are from an extremely diverse community (e.g. soil)
+dadaFs = dada(filtFs, err = errF, pool = FALSE, multithread = TRUE)
+dadaRs = dada(filtRs, err = errR, pool = FALSE, multithread = TRUE)
 
 # Merge paired reads (Default: minOverlap = 20; maxMismatch = 0)
-mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE)
+mergers = mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose = TRUE)
 
 # Construct a sample-by-sequence observation matrix
-seqtab <- makeSequenceTable(mergers)
+seqtab = makeSequenceTable(mergers)
+
+table(nchar(getSequences(seqtab)))
 
 # Save sequence table
 saveRDS(seqtab, "seqtab.rds") # or as an example, use seqtab[c(1:5),] to save data for a subset of 5 samples
